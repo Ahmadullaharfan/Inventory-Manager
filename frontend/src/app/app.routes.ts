c41e193f9a0d7007e1496ff060e2fdf32ce9338d
Ahmadullah_Arfan <irfanahmadullah@gmail.com>
2026-09-20 09:23:54 +0430

    some terminal brwoser support warning issue solved

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -10,37 +10,45 @@ export const appRoutes: Routes = [
     component: HomeComponent,
     data: { animation: 'home' }
   },
+
   {
     path: 'products',
     component: Index,
     data: { animation: 'sample' }
   },
+
   {
     path: 'products/create',
     component: ProductForm,
     data: { animation: 'sample' }
   },
+
   {
     path: 'products/edit/:id',
     component: ProductForm,
     data: { animation: 'sample' }
   },
+
   {
     path: 'pages',
     loadChildren: () => import('./main/pages/pages.module').then(m => m.PagesModule)
   },
+
   {
     path: 'productCatagory',
     loadChildren: () =>
       import('./main/features/ProductCatagory/product-catagory.routes').then(m => m.PRODUCT_CATEGORY_ROUTES)
   },
+
   {
     path: '',
     redirectTo: '/home',
     pathMatch: 'full'
   },
+
   {
     path: '**',
     redirectTo: '/pages/miscellaneous/error'
   }
+  
 ];
